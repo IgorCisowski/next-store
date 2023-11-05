576fd054f79c7ff6ba070643ca1471a6686fbc9d
IgorCisowski <igorcisowski2098@gmail.com>
2023-11-05 12:11:16 +0000

    feat: routes, products, product page

---
+++ app/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="py-5 flex justify-center w-full">
+      <p>© LOGO {new Date().getFullYear()} | All rights Reserved</p>
+    </footer>
+  );
+}
